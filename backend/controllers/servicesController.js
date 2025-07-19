cc43883e4058203f0422743e0e3300cdeec0e4e9
Hareeswar <hareeswar0816@gmail.com>
2025-07-19 23:18:50 +0530

    Added the plc page

--- backend/controllers/servicesController.js
+++ backend/controllers/servicesController.js
@@ -1,3 +1,7 @@
 export const getServices = (req, res) => {
   res.render("services");
 };
+
+export const getPrivateLimitedPage = (req, res) => {
+  res.render('services/plc');
+};
--- backend/routes/servicesRoute.js
+++ backend/routes/servicesRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getServices } from "../controllers/servicesController.js";
+import { getServices,getPrivateLimitedPage } from "../controllers/servicesController.js";
 
 const router = express.Router();
 router.get("/services", getServices);
+router.get("/services/private-limited-company", getPrivateLimitedPage);
 
 export default router;
